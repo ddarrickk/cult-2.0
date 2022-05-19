e0618904c770c9d3ad5c62c59c6b0e61e20d048d
Darrick Chu PC <ddarrickk@gmail.com>
2022-05-19 09:53:14 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Backdrop from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/Back drop2.png'
+import ult from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/ult2.png'
+import initiation from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/initiation.png'
+import initiation2 from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/inisheashon.png'
+import Backdrop2 from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/Back drop side2.png'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <div>
+            <div className={'parent'}>
+                <img className={'backdrop'} src={Backdrop}/>
+                <img className={'backdropSide'} src={Backdrop2}/>
+                <img className={'initiation'} src={initiation}/>
+                <img className={'initiation2'} src={initiation2}/>
+                <img className={'ult'} src={ult}/>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
