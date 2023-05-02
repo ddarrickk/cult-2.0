4cad734d84464e48241796f23dcb8108a9885906
Darrick Chu PC <ddarrickk@gmail.com>
2023-05-02 11:03:31 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Backdrop from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/Back drop2.png'
 import ult from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/ult2.png'
-import initiation from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/initiation.png'
+import initiation from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/Initiation2.png'
 import initiation2 from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/inisheashon.png'
 import Backdrop2 from 'C:/Users/sundi/Documents/Coding/JS/cult/src/Site Objects/Back drop side2.png'
 import './App.css'
@@ -12,7 +12,7 @@ const App = () => {
                 <img className={'backdrop'} src={Backdrop}/>
                 <img className={'backdropSide'} src={Backdrop2}/>
                 <img className={'initiation'} src={initiation}/>
-                <img className={'initiation2'} src={initiation2}/>
+                {/* <img className={'initiation2'} src={initiation2}/> */}
                 <img className={'ult'} src={ult}/>
             </div>
         </div>
